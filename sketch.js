899d2549bc3a41ef2e367f3b89e0d0d0c6c67260
laural21 <lauralehoczki@gmail.com>
2017-12-03 12:47:14 -0500

    Included images, slicing looks good to me

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var animals = [];
 var a1, a2, a3, a4;
 var randomAnimal;
 
+
 // wanderer array
 var theWanderers = [];
 
@@ -16,7 +17,7 @@ var countdown;
 var win;
 var lose;
 
-//
+// 
 var faces = [];
 var currentFaces;
 
@@ -29,10 +30,10 @@ function Face(){
 }
 
 function preload(){
-  a1 = loadImage("");
-  a2 = loadImage("");
-  a3 = loadImage("");
-  a4 = loadImage("");
+  a1 = loadImage("images/elephant.png");
+  a2 = loadImage("images/fox.png");
+  a3 = loadImage("images/gorilla.png");
+  a4 = loadImage("images/raccoon.png");
   win = loadSound();
   lose = loadSound();
   countdown = loadSound();
@@ -62,7 +63,6 @@ function setup() {
       }
     }
   }
-
   noFill();
   stroke(255);
   strokeWeight(1);
@@ -83,7 +83,7 @@ function getRandom(){
 
 var targetAnimal = getRandom();
 
-if (mouseClicked()){
+if mouseClicked(){
   for(var i = 0; i < faces.length; i++){
     if(dist(mouseX, mouseY, faces[i].xPos, faces[i].yPos) < 5){
       face.animal = getRandom();
@@ -104,8 +104,8 @@ function draw() {
   world.clearDrawingCanvas();
   stroke(0,255,0);
   fill(25, 16, 8, 10); // Color filter over the world
-  rect(0,0,width,height);
-
+  rect(0,0,width,height); 
+  
   stroke(0);
   text(world.video.style.height, 25, 25);
 
@@ -117,14 +117,15 @@ function draw() {
 
   fill(255);
   text(timer/60, 10, 10);
-
+  
   // Display the target animal
   image(targetAnimal, 10, height-10, 30, 30);
-
+  
   for (var i = 0; i < currentFaces.length; i++) {
     image(faces[i].animal, currentFaces[i].x, currentFaces[i].y, currentFaces[i].width, currentFaces[i].height);
   }
 
+  
   if(timer > 0 && checkForWin() == true){
       win.play();
       // write separate function for image slicing and do it here
@@ -191,13 +192,12 @@ function Wanderer(x, y, myImage) {
 }
 
 
-
 // hacked together code to make this work with p5 below - no need to change this
 // unless you want to dig deeper into the system and make it behave differently
 function World(facingMode, w, h) {
   this.w = w;
   this.h = h;
-
+  
   // create video
   this.video = document.createElement('video');
   this.video.style.width = w+'px';
@@ -233,11 +233,11 @@ function World(facingMode, w, h) {
   // set up face position array
   this.facePositions = [];
   this.rawFacePositions = [];
-
+  
   this.tracker.on('track', function(event) {
     _this.rawFacePositions = [];
   });
-
+  
   this.FaceRecord = function(x,y,w,h) {
     this.x = x;
     this.y = y;
