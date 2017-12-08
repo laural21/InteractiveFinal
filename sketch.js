cc0a822d040fd1d39e7666f3ca88cb69e6295c8b
laural21 <lauralehoczki@gmail.com>
2017-12-08 18:05:24 -0500

    Displays animal over face

--- sketch.js
+++ sketch.js
@@ -19,14 +19,14 @@ var lose;
 
 //
 var faces = [];
-var currentFaces;
+var currentFaces = [];
 
 // Depending on how many faces are detected, create Face objects and add them to faces[]
-function Face(){
+function Face(x, y){
   this.index;
-  this.animal = animals[random(4)];
-  this.xPos;
-  this.yPos;
+  this.animal = random(animals);
+  this.xPos = x;
+  this.yPos = y;
 }
 
 function preload(){
@@ -36,12 +36,15 @@ function preload(){
   a4 = loadImage("images/raccoon.png");
   //win = loadSound();
   //lose = loadSound();
-  //countdown = loadSound("sounds/countdown.ogg");
+  countdown = loadSound("sounds/countdown.ogg");
 }
 
 function setup() {
   world = new World('front', 640, 480); // 'front' or 'user' - sets camera preference
 
+  noFill();
+  stroke(255);
+  strokeWeight(1);
   // randomize perlin noise landscape
   noiseDetail(24);
 
@@ -63,29 +66,21 @@ function setup() {
       }
     }
   }
-  noFill();
-  stroke(255);
-  strokeWeight(1);
   animals.push(a1);
   animals.push(a2);
   animals.push(a3);
   animals.push(a4);
   targetAnimal = getRandom();
-  currentFaces = world.getRawFacePositions();
-  for (var i = 0; i < currentFaces.length; i++) {
-    faces.push(new Face());
-  }
 }
 
 function getRandom(){
   return random(animals);
 }
 
-
 function mouseClicked(){
   for(var i = 0; i < faces.length; i++){
-    if(dist(mouseX, mouseY, faces[i].xPos, faces[i].yPos) < 5){
-      face.animal = getRandom();
+    if(dist(mouseX, mouseY, faces[i].xPos, faces[i].yPos) < 20){
+      faces[i].animal = getRandom();
     }
   }
 }
@@ -98,6 +93,14 @@ function checkForWin(){
   }
   return true;
 }
+// countdown function
+function decreaseTime() {
+  counter -- ;
+  if (counter == 0) {
+    counterInSeconds -= 1;
+    counter = 8;
+  }
+}
 
 function draw() {
   world.clearDrawingCanvas();
@@ -105,22 +108,31 @@ function draw() {
   fill(25, 16, 8, 10); // Color filter over the world
   rect(0,0,width,height);
 
-  stroke(0);
-
-
   fill(255);
-  text(counterInSeconds, 10, 10);
+  if (counterInSeconds >= 0){
+  	text(counterInSeconds, 30, 30);
+  } else {
+  	text("0", 30, 30);
+  }
+  
+
+  currentFaces = world.getRawFacePositions();
+  while (faces.length < currentFaces.length){
+  	for (var i = 0; i < currentFaces.length; i++) {
+    faces.push(new Face(currentFaces[i].x, currentFaces[i].y));
+  	}
+  }
 
   // Display the target animal
   imageMode(CENTER);
   image(targetAnimal, 40, height-20, 40, 40);
 
+  imageMode(CORNER);
   for (var i = 0; i < currentFaces.length; i++) {
-    image(faces[i].animal, currentFaces[i].x, currentFaces[i].y, currentFaces[i].width, currentFaces[i].height);
+    image(faces[i].animal, currentFaces[i].x, currentFaces[i].y, currentFaces[i].width + 50, currentFaces[i].height + 50);
   }
 
-  countdown();
-
+  decreaseTime();
 
   if(counterInSeconds > 0 && checkForWin() == true){
       //win.play();
@@ -145,16 +157,6 @@ function draw() {
     //countdown.play();
   }
 
-  console.log(currentFaces);
-}
-
-// countdown function
-function countdown() {
-  counter -- ;
-  if (counter == 0) {
-    counterInSeconds -= 1;
-    counter = 8;
-  }
 }
 
 function Wanderer(x, y, myImage) {
@@ -242,69 +244,15 @@ function World(facingMode, w, h) {
   this.rawFacePositions = [];
 
   this.tracker.on('track', function(event) {
-    _this.rawFacePositions = [];
+    _this.rawFacePositions = event.data;
   });
 
-  this.FaceRecord = function(x,y,w,h) {
-    this.x = x;
-    this.y = y;
-    this.width = w;
-    this.height = h;
-    this.age = 0;
-    this.cache = [];
-
-    this.resetAge = function() {
-      this.age = 0;
-    }
-
-    this.getDist = function(e) {
-      return dist(this.x, this.y, e.x, e.y);
-    }
-
-    this.refreshPosition = function(e) {
-      this.cache.push([e.x, e.y, e.width, e.height]);
-      if (this.cache.length > 5) {
-        this.cache.splice(0,1);
-      }
-      var xAvg = 0;
-      var yAvg = 0;
-      var wAvg = 0;
-      var hAvg = 0;
-      for (var i = 0; i < this.cache.length; i++) {
-        xAvg += this.cache[i][0];
-        yAvg += this.cache[i][1];
-        wAvg += this.cache[i][2];
-        hAvg += this.cache[i][3];
-      }
-      this.x = xAvg/this.cache.length;
-      this.y = yAvg/this.cache.length;
-      this.width = wAvg/this.cache.length;
-      this.height = hAvg/this.cache.length;
-      this.age = 0;
-    }
-
-    this.ageUp = function() {
-      this.age++;
-      if (this.age > 10) {
-        return true;
-      }
-      return false;
-    }
-  }
-
-  // public methods for getting face positions
-  this.getFacePositions = function() {
-    var returnArray = [];
-    for (var i = 0; i < this.facePositions.length; i++) {
-      returnArray.push( this.facePositions[i] );
-    }
-    return returnArray;
-  }
   this.getRawFacePositions = function() {
     var returnArray = [];
     for (var i = 0; i < this.rawFacePositions.length; i++) {
       returnArray.push( this.rawFacePositions[i] );
     }
+    console.log(returnArray);
     return returnArray;
   }
 
